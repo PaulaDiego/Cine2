adbafde048d36c34ea95c8b39af234076f70d0ed
Paula <paulacursocic@gmail.com>
2017-08-24 17:49:55 +0200

    using de cors

--- Cine/Controllers/PeliculasController.cs
+++ Cine/Controllers/PeliculasController.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
+﻿using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Description;
-using Cine;
 using Cine.Models;
+using System.Web.Http.Cors;
 
 namespace Cine.Controllers
 {
